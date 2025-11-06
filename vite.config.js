061c69128dc92c81bb865db25ca0a7496c1df4bd
JanAdriaan01 <janadriaan@lethabocabling.co.za>
2025-11-06 08:13:19 +0200

    remove analytics

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['**/*.jsx?'] // This helps with JSX files
+  build: {
+    outDir: 'dist'
   }
 })
