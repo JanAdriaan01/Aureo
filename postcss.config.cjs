82fcd05f3c874c3ca52da74b217a9c2fb3eb18c1
JanAdriaan01 <janadriaan@lethabocabling.co.za>
2025-11-07 15:01:25 +0200

    Remove local Vercel build artifacts; use Vercel remote build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,21 @@
-﻿// postcss.config.js
-export default {
+﻿# PostCSS config -> CJS
+Remove-Item postcss.config.js -ErrorAction Ignore
+@'
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+'@ | Set-Content -Encoding UTF8 postcss.config.cjs
+
+# Tailwind config -> CJS
+Remove-Item tailwind.config.js -ErrorAction Ignore
+@'
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
+'@ | Set-Content -Encoding UTF8 tailwind.config.cjs
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-@'
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
-'@ | Set-Content -Encoding UTF8 tailwind.config.js
