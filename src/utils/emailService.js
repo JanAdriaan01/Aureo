63d0d682776f90e685183c91d905e5f9456f2dc6
JanAdriaan01 <janadriaan@lethabocabling.co.za>
2025-11-16 01:39:08 +0200

    V8

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -1,4 +1,11 @@
-// src/utils/emailService.js
-export function sendOrderEmail(orderData) {
-  console.log("Pretend we sent email:", orderData);
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function sendOrderEmail(data) {
+  const res = await fetch(`${API_URL}/api/send-email`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
 }
